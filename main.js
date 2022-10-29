c33955b1627dff4a4db31a1064ba6979e35ca525
Code_For_Live <107235653+CodeWithMe-FPT@users.noreply.github.com>
2022-10-29 21:43:46 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+const checkAccount = async(e)=> {
+    var email = document.querySelector('.input-email').value;
+    var pass = document.querySelector('.input-password').value
+    e.preventDefault();
+    let status  = await getAccountStatus(email, pass);
+    if(!status){
+        location.reload();
+    } else{
+        if(status == "success"){
+            console.log(status)
+        } else{
+            if(status == "failed"){
+                console.log(status)
+            } else{
+                console.log(status);
+            }
+        }
+    }
+}
+document.querySelector('.submit-account').addEventListener('submit', checkAccount);
